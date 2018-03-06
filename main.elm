25214e048a001e3f48ead856c09192f754465bec
Drew Lazzeri <drew@interviewing.io>
2018-03-05 21:07:00 -0800

    Better issues

--- main.elm
+++ main.elm
@@ -6,41 +6,96 @@ import Html exposing (Html, div, text)
 import List
 import String
 
+
 main : Html msg
-main = let parse = run parser Org.source
-       in case parse of
-              Ok lines -> displayOrg lines
-              Err issue -> displayIssue issue
+main =
+    let
+        parse =
+            run parser Org.source
+    in
+        case parse of
+            Ok lines ->
+                displayOrg lines
 
-type Piece = Heading String | OtherLine
+            Err issue ->
+                displayIssue issue
 
-displayIssue {row, col, source, problem, context} = let sourceLines= String.split "\n" source 
-                                                    in div [] <| (text <| "Error at row " ++ (toString row) ++ " and column " ++ (toString col)) :: List.map (\x -> div [] [text x]) sourceLines
+
+type Piece
+    = Heading String
+    | OtherLine
+
+stringifyProblem : Problem -> String
+stringifyProblem p = case p of
+                         BadOneOf problems -> String.join " && " <| List.map stringifyProblem problems
+                         BadInt -> "Bad Int"
+                         BadFloat -> "Bad Float"
+                         BadRepeat -> "Bad Repeat"
+                         ExpectingEnd -> "Expecting End"
+                         ExpectingSymbol s -> "Expecting Symbol_" ++ s ++ "_"
+                         ExpectingKeyword k -> "Expecting Keyword_" ++ k ++ "_"
+                         ExpectingVariable -> "Expecting Variable"
+                         ExpectingClosing c -> "Expecting Closing_" ++ c ++ "_"
+                         Fail f -> "Fail_" ++ f ++ "_"
+
+displayIssue : Error -> Html msg
+displayIssue { row, col, source, problem, context } =
+    let
+        sourceLines =
+            String.split "\n" source
+
+        body = List.map (\x -> div [] [ text x ]) sourceLines
+        heading =  "Error at row "
+                   ++ toString row
+                   ++ " and column "
+                   ++ toString col
+                   ++ ": "
+                   ++ stringifyProblem problem
+                   |> text 
+    in
+        heading :: body |> div []
 
 displayOrg : List Piece -> Html msg
-displayOrg pieces = List.map displayPiece pieces
-             |> div []
+displayOrg pieces =
+    List.map displayPiece pieces
+        |> div []
+
 
 displayPiece : Piece -> Html msg
-displayPiece p = case p of
-                     Heading h -> div [] [text h]
-                     OtherLine -> div [] []
+displayPiece p =
+    case p of
+        Heading h ->
+            div [] [ text h ]
+
+        OtherLine ->
+            div [] []
+
 
 parser : Parser (List Piece)
-parser = repeat oneOrMore lines
+parser =
+    repeat oneOrMore lines
+
 
 lines : Parser Piece
-lines = oneOf [ headerLine
-              , tabledLine
-              , succeed OtherLine |. end
-              ]
+lines =
+    oneOf
+        [ headerLine
+        , tabledLine
+        , succeed OtherLine
+            |. ignore zeroOrMore (\x -> True)
+            |. end
+        ]
+
 
 headerLine : Parser Piece
-headerLine = succeed Heading
-           |. ignore oneOrMore (\x -> x == '*')
-           |= keep oneOrMore (\x -> x /= '\n')
-           |. symbol "\n"
+headerLine =
+    succeed Heading
+        |. ignore oneOrMore (\x -> x == '*')
+        |= keep oneOrMore (\x -> x /= '\n')
+        |. symbol "\n"
+
 
 tabledLine : Parser Piece
-tabledLine = succeed OtherLine
-           |. ignoreUntil "\n"
+tabledLine =
+    succeed OtherLine
+        |. ignoreUntil "\n"
